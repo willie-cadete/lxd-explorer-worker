c66276927f4f243eb8b68f17834024b47cc3f2c4
Willie Cadete <williecadete@gmail.com>
2020-04-23 21:11:52 +0100

    Add documentation with yard

--- lib/database.rb
+++ lib/database.rb
@@ -1,7 +1,12 @@
 require 'logger'
 require 'redis'
 
+# Class to provides methods regading Database
 class Database
+  # Returns a new instance of Database
+  # @param host [String] The Redis address
+  # @param port [Number] port number
+  # @param loglevel [String] Set the loglevel
   def initialize(host, port = 6379, loglevel = 'info')
     @logger = if ENV['APP_ENV'] == 'test'
                 Logger.new('/dev/null')
@@ -14,11 +19,16 @@ class Database
     connect
   end
 
+  # Create a Redis object
   def connect
     @client = Redis.new(host: @host, port: @port)
     @logger.info("Connecting to database #{@host}:#{@port}")
   end
 
+  # Save the container information into Redis
+  # @param name [String] Define the name of a hash key
+  # @param expire [Number] Set the time to key expiration
+  # @param data [Hash] The keyworkds to be stored into Redis
   def save_container(name, expire, **data)
     data.each do |k, v|
       @client.hset(name, k, v)
--- lib/lxd.rb
+++ lib/lxd.rb
@@ -1,9 +1,15 @@
 require 'hyperkit'
 require 'logger'
 
+# Class to provides methods regading Database
 class Lxd
   attr_reader :api_endpoint
 
+  # Returns a new instance of Lxd
+  # @param api_endpoint [String] The base URL for API requests
+  # @param client_cert [String] The client certificate used to authenticate to the LXD server.
+  # @param client_key [String] The client key used to authenticate to the LXD server.
+  # @param loglevel [String] The loglevel
   def initialize(api_endpoint, client_cert, client_key, loglevel = 'info')
     @logger = if ENV['APP_ENV'] == 'test'
                 Logger.new('/dev/null')
@@ -17,6 +23,7 @@ class Lxd
     connect
   end
 
+  # Create the conenction object
   def connect
     @client = Hyperkit::Client.new(
       client_cert: @client_cert,
@@ -29,6 +36,8 @@ class Lxd
     @logger.debug "Connection settings: #{@client.inspect}"
   end
 
+  # Returns a list of containers from LXD host
+  # @return [Array]
   def get_containers
     @logger.debug "#{@client.api_endpoint} - Listing containers: #{@client.containers}"
     @client.containers
@@ -37,6 +46,9 @@ class Lxd
     []
   end
 
+  # Return the information about a container
+  # @param container [String] The container's name
+  # @return [Hash]
   def get_container_info(container)
     @logger.debug "#{@client.api_endpoint} - Container info: #{@client.container(container).to_h}"
     @client.container(container).to_h
@@ -45,6 +57,9 @@ class Lxd
     {}
   end
 
+  # Return container state information
+  # @param container [String] The container's name
+  # @return [Hash]
   def get_container_state(container)
     @logger.debug "#{@client.api_endpoint} - Container state: #{@client.container_state(container).to_h}"
     @client.container_state(container).to_h
