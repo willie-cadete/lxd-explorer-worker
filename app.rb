1bb73c735dda0439d6c96b627db56bfb9be671a3
Willie Cadete <williecadete@gmail.com>
2020-04-30 09:59:05 +0100

    Add total time, measuare time for the whole importation

--- app.rb
+++ app.rb
@@ -11,24 +11,29 @@ logger.info('Application has been started.')
 
 servers = ENV['LXD_HOSTS'].split(',')
 
+# TODO: Refactor this mess
+
 loop do
-  servers.each do |s|
-    lxd = Lxd.new(s, ENV['CLIENT_CERT'], ENV['CLIENT_KEY'], ENV['LOG_LEVEL'])
-    redis = Database.new(ENV['REDIS_HOST'], ENV['REDIS_PORT'], ENV['LOG_LEVEL'])
-    
-    time = Benchmark.realtime do
-      lxd.get_containers.each do |container|
-        redis.save_container(
-          "lxd:#{URI.parse(lxd.api_endpoint).hostname}:#{container}",
-          ENV['INTERVAL'].to_i + 60,
-          info: lxd.get_container_info(container).to_json,
-          state: lxd.get_container_state(container).to_json
-        )
+  total_time = Benchmark.realtime do
+    servers.each do |s|
+      lxd = Lxd.new(s, ENV['CLIENT_CERT'], ENV['CLIENT_KEY'], ENV['LOG_LEVEL'])
+      redis = Database.new(ENV['REDIS_HOST'], ENV['REDIS_PORT'], ENV['LOG_LEVEL'])
+      
+      import_time = Benchmark.realtime do
+        lxd.get_containers.each do |container|
+          redis.save_container(
+            "lxd:#{URI.parse(lxd.api_endpoint).hostname}:#{container}",
+            ENV['INTERVAL'].to_i + 60,
+            info: lxd.get_container_info(container).to_json,
+            state: lxd.get_container_state(container).to_json
+          )
+        end
       end
+      logger.info('Containers data have been saved')
+      logger.info("Import time: #{import_time.round(2)}s")
     end
-    logger.info("Import time: #{time.round(2)}s")
-    logger.info('Containers data have been saved')
   end
+  logger.info("Total import time: #{total_time.round(2)}s")
 
   sleep ENV['INTERVAL'].to_i
 end
